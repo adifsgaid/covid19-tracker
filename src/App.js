34502bebdb3c604edbb2a6ea83fbdc97b9eb9668
adifsgaid <infosgaid@gmail.com>
2020-12-16 09:51:19 +0100

    added container app.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import {MenuItem, Select, FormControl, Card, CardContent,} from '@material-ui/co
 function App() {
   return (
     <div className="app">
-      <h1>Covid-19 Tracker</h1>
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
         <FormControl className='app__dropdown'>
-          <Select
-            variant='outlined'
-            value='abc'>
-            
-            <MenuItem value='worldide'>worldide</MenuItem>
-            <MenuItem value='worldide'>worldide</MenuItem>
-            <MenuItem value='worldide'>worldide</MenuItem>
-          
-          </Select>
-        </FormControl>
+            <Select variant='outlined' value='abc'>
+              <MenuItem value='worldide'>worldide</MenuItem>
+              <MenuItem value='worldide'>worldide</MenuItem>
+              <MenuItem value='worldide'>worldide</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
         {/* header*/}
         {/* title + dropDown */}
 
