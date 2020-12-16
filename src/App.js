58e3ff33c85e5fc186b2a3ba1e233b9b849c7d3c
adifsgaid <infosgaid@gmail.com>
2020-12-16 21:34:40 +0100

    fixed bug function on change the object returned was the wrong one

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ import Map from './components/Map'
 function App() {
  
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('worldide');
+  const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({})
   
+  useEffect(()=>{
+    fetch('https://disease.sh/v3/covid-19/all').then(response => response.json()).then(data =>{
+      setCountryInfo(data);
+    })
+  },[]) 
+
   useEffect(() => {
     const getCountriesData = async() => {
       await fetch("https://disease.sh/v3/covid-19/countries").then((response)=> response.json()).then((data)=>{
         const countries = data.map((country) =>({
           name: country.country,
-          value: country.countryInfo.iso2
+          value: country.countryInfo.iso2,
         }));
         setCountries(countries);
       })
@@ -25,10 +31,14 @@ function App() {
  
   const onCountryChange = async (e) => {
     const countryCode = e.target.value;
+    
     setCountry(countryCode)
-    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+    const url =
+    countryCode === "worldwide"
+      ? "https://disease.sh/v3/covid-19/all"
+      : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
     await fetch(url).then(response => response.json()).then(data => {
-      setCountry(data);
+      setCountry(countryCode);
       setCountryInfo(data);
     })
   }
@@ -40,25 +50,26 @@ function App() {
         <h1>Covid-19 Tracker</h1>
         <FormControl className='app__dropdown'>
             <Select variant='outlined' onChange={onCountryChange} value={country}>
-            <MenuItem value='worldide'>worldide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
               {
                 countries.map((country)=>
                 (<MenuItem value={country.value}>{country.name}</MenuItem>))
               }
             </Select>
           </FormControl>
-        </div>
+        </div> 
 
         <div className='app__stats'>
-          <InfoBox title='Coronavirus cases' cases={5000}  total={500}/>
-          <InfoBox title='Recovered' cases={5000} total={500}/>
-          <InfoBox title='Deaths' cases={5000} total={500} />
+          <InfoBox title='Coronavirus cases' cases={countryInfo.todayCases} total={countryInfo.cases}/>
+          <InfoBox title='Recovered' cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+          <InfoBox title='Deaths' cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
         </div>
         <Map/>
     </div>
-     <Card className='app__right'>
-      <CardContent>
+    <Card className='app__right'>
+     <CardContent>
         <h3>Live Cases By Country</h3>
+        <Table countries={tableData}/>
         <h3>Worldwide new cases</h3>
       </CardContent>
         {/* graph */}
