9004f663a0c8e4dd23d84e8545bc88aa79074cc0
adifsgaid <infosgaid@gmail.com>
2020-12-16 09:39:06 +0100

    added material ui dependencies" git add .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ function App() {
   return (
     <div className="app">
       <h1>Tracking covid19</h1>
+        
+        {/* header*/}
+        {/* title + dropDown */}
+
+        {/* infoBoxes */}
+        {/* infoBoxes */}
+        {/* infoBoxes */}
+        
+        {/* table */}
+        
+        {/* graph */}
+        
+        {/* map */}
+    
     </div>
   );
 }
