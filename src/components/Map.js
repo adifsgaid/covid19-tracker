e431c0637ab5e77f685c86aa878ed53a10f31e19
adifsgaid <infosgaid@gmail.com>
2020-12-17 14:15:01 +0100

    need to fix zoom

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MapContainer, TileLayer } from 'react-leaflet';
+import {MapContainer as MapLeaflet, TileLayer } from 'react-leaflet';
 import './Map.css'
 import 'leaflet/dist/leaflet.css';
 
@@ -7,12 +7,12 @@ import 'leaflet/dist/leaflet.css';
 function Map({ center, zoom }){
     return (
         <div className='map'>
-          <MapContainer center={center} zoom={zoom}>
+          <MapLeaflet center={center} zoom={zoom}>
             <TileLayer
              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-         </MapContainer>
+         </MapLeaflet>
         </div>
     )
 }
