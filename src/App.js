f9fc51fb5f8459bf5353e895303e896d3166c853
adifsgaid <infosgaid@gmail.com>
2020-12-18 11:50:06 +0100

    changed map with MAPBOX but still need to fix it

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Map from './components/Map';
 import Table from './components/Table';
 import { sortData } from './components/util';
 import LineGraph  from './components/LineGraph';
-import 'leaflet/dist/leaflet.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
  
@@ -18,43 +18,50 @@ function App() {
   const [mapZoom, setMapZoom] = useState(3);
 
   
-  useEffect(()=>{
-    fetch('https://disease.sh/v3/covid-19/all').then(response => response.json()).then(data =>{
-      setCountryInfo(data);
-    })
-  },[]);
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   useEffect(() => {
-    const getCountriesData = async() => {
-      await fetch("https://disease.sh/v3/covid-19/countries").then((response)=> response.json()).then((data)=>{
-        const countries = data.map((country) =>({
-          name: country.country,
-          value: country.countryInfo.iso2,
-        }));
-        const sortedData = sortData(data)
-        setTableData(sortedData); 
-        setCountries(countries);
-      })
-    }
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          let sortedData = sortData(data);
+          setCountries(countries);
+          setTableData(sortedData);
+        });
+    };
+
     getCountriesData();
   }, []);
 
   const onCountryChange = async (e) => {
     const countryCode = e.target.value;
-  
+
     const url =
-    countryCode === "worldwide"
-      ? "https://disease.sh/v3/covid-19/all"
-      : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
-    await fetch(url).then(response => response.json()).then(data => {
-      setCountry(countryCode);
-      setCountryInfo(data);
-      
-      setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
-      setMapZoom(4);
-    })
-  }
-  
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+        console.log(data.countryInfo.lat, data.countryInfo.long)
+        setMapCenter([ data.countryInfo.lat, data.countryInfo.long ]);
+        setMapZoom(4);
+      });
+  };
+
   return (
     <div className="app">
      <div className='app__left'>
@@ -79,6 +86,7 @@ function App() {
         <Map
           center={mapCenter}
           zoom={mapZoom}/>
+    
     </div>
     <Card className='app__right'>
      <CardContent>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import {MapContainer as MapLeaflet, TileLayer } from 'react-leaflet';
+import React, {useEffect,useRef} from 'react';
 import './Map.css'
-import 'leaflet/dist/leaflet.css';
-
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoibm90YWxlbWVzYSIsImEiOiJjazhiOTZnb2gwM3NxM2ZucGp1Z21mNjZ0In0.Z4nS6wdB4WzflkDItyXSIQ";
 
 function Map({ center, zoom }){
-    return (
-        <div className='map'>
-          <MapLeaflet center={center} zoom={zoom}>
-            <TileLayer
-             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-         </MapLeaflet>
+    const mapboxElRef = useRef(null); // DOM element to render map
+
+  // Initialize our map
+  useEffect(() => {
+    // Mapbox functionality goes here
+    new mapboxgl.Map({
+      container: mapboxElRef.current,
+      style: "mapbox://styles/notalemesa/ck8dqwdum09ju1ioj65e3ql3k",
+      center: center,
+      zoom: zoom, // initial zoom
+    });
+  }, []);
+    
+  return (
+        <div className='map' >
+            <div className='mapContainer'>
+                <div className="mapBox" ref={mapboxElRef} />
+            </div>
         </div>
     )
 }
