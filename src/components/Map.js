de3c3928b3d87c0a7136ecc60af5340936149a4e
adifsgaid <infosgaid@gmail.com>
2020-12-21 13:02:34 +0100

    fixed issue with map to render heat map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, {useEffect,useRef} from 'react';
 import './Map.css'
 import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MapToken = ReactMapboxGl({
-  accessToken:
-    'pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A'
-});
+const MapToken = ReactMapboxGl ({accessToken:`${process.env.REACT_APP_API_KEY_MAPBOX}`});
 
 function Map({ center, zoom }){
   return (
         <div className='map'>
-           <MapToken
-              style="mapbox://styles/mapbox/streets-v9"
-              containerStyle={{
-                height: '100%',
-                width: '100%',
-                center: center,
-                zoom: zoom
-              }}
-            >
-            </MapToken> 
+            <div className='mapContainer'>
+                <div className="mapBox" >
+                <MapToken
+                    style="mapbox://styles/mapbox/streets-v9"
+                    containerStyle={{
+                      height: '100%',
+                      width: '100%',
+                    }}
+                    zoom={[zoom]}
+                    center={center}
+                    >              
+                  </MapToken>
+                  </div>
+            </div>
         </div>
     )
 }
