15c4b4af8d67a7ad23d33b2ba38579ca4b166a6b
adifsgaid <infosgaid@gmail.com>
2020-12-26 10:47:58 +0100

    added modif

---
+++ src/environment.js
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+  
+    mapbox: {
+      accessToken: "pk.eyJ1IjoiYWRpZnNnYWlkIiwiYSI6ImNrajJ1eHJ3ZTNlbzcyeHNjbDY2Zmh2Z3QifQ.cQCAHidfuKgWPuJ_BTRCFw"
+    }
+  }
